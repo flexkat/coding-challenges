920049941c1b05cbb04c5836a906fd8ed5c7d8c3
flexkat <catherinemaritz1@gmail.com>
2019-09-18 21:17:12 +0100

    flight time

---
+++ src/hackerRank/Interview Prep/movie-lengths.js
@@ -0,0 +1,41 @@
+const moviesForPlaneRideOne = (flightTime, movies) => {
+  for(let i = 0; i < movies.length; i++) {
+    for(let j=0; j < movies.length; j++){
+      if (movies[i] + movies[j] === flightTime && i !== j) return true 
+    }
+  }
+  return false
+}
+// this version works but is Osquared, ideal to not use a nested loop. 
+
+const moviesForPlaneRideTwo = (flightTime, movies) => {
+  let movieObj = {}
+  movies.map((movie, i) => movieObj[movie] = i)
+  for(let i = 0; i < movies.length; i++) {
+    let diff = flightTime - movies[i]
+
+    let movieTwo = movieObj[diff]
+
+    if(movieTwo !== undefined && movieTwo !== i) return true
+
+  }
+  return false
+}
+// here we've utilised a data structure to make comparing easier, to find what we are looking for without going through each element in an array - by transforming our array into an object with the data.
+//There is still an optimization that could be made though, as we still have a 2O with the map outside it.
+
+
+const moviesForPlaneRideThree = (flightTime, movies) => {
+  let movieObj = {}
+  for(let i = 0; i < movies.length; i++) {
+    let diff = flightTime - movies[i]
+    movieObj[diff] = true
+    console.log(movieObj)
+
+    if(diff == movies[i]) return true
+
+  }
+  return false
+}
+const movieArray = [80, 50, 80]
+const flightTime = 160
